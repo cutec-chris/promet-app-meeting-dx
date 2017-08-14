816eb5ebb1edd6623c7e8eb9409e77521b7be0fb
Christian Ulrich <info@cu-tec.de>
2017-08-14 18:24:01 +0200

    Move to Form Classes

--- meeting.js
+++ meeting.js
@@ -1,21 +1,9 @@
-var siTimes,tbToolbar,gTimes,dsTimes;
+var Meetings;
 dhtmlxEvent(window,"load",function(){
-  console.log("Loading Meeting Page...");
-  sbMain.addItem({id: 'siMeeting', text: 'Besprechungen', icon: 'fa fa-refresh'});
-  siMeeting = window.parent.sbMain.cells('siMeeting');
-  tbToolbar = siMeeting.attachToolbar({
-    parent:"pToolbar",
-      items:[
-         {id: "new", type: "button", text: "Neu", img: "fa fa-plus-circle"}
-        ,{id: "sep1", type: "separator" }
-        ,{id: "sep1", type: "separator" }
-        ,{id: "refresh", type: "button", text: "Aktualisieren", img: "fa fa-refresh"}
-      ],
-    iconset: "awesome"
-  });
-  tbToolbar.attachEvent("onClick", function(id) {
-    if (id=='new') {
-    } else if (id=='refresh') {
-    }
-  });
+  Meetings = newPrometList('meetings','Besprechungen');
+  Meetings.Grid.setHeader(["Nummer","Name","Status","ersteller"]);
+  Meetings.Grid.setColumnIds('ID,NAME,STATUS,CREATEDBY')
+  Meetings.Grid.setColTypes("txt,txt,txt,txt");
+  Meetings.Grid.attachHeader("#text_filter,#text_filter,#select_filter,#text_filter");
+  Meetings.Grid.init();
 });
