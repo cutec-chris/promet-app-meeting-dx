6fcbcbf669934627f9c995971825b1d8ddef9beb
Christian Ulrich <info@cu-tec.de>
2017-08-15 16:20:23 +0200

    right Headers

--- meeting.js
+++ meeting.js
@@ -1,9 +1,9 @@
 var Meetings;
 dhtmlxEvent(window,"load",function(){
   Meetings = newPrometList('meetings','Besprechungen');
-  Meetings.Grid.setHeader(["Nummer","Name","Status","ersteller"]);
-  Meetings.Grid.setColumnIds('ID,NAME,STATUS,CREATEDBY')
+  Meetings.Grid.setHeader(["Name","Status","Datum","ersteller"]);
+  Meetings.Grid.setColumnIds('NAME,STATUS,DATE,CREATEDBY')
   Meetings.Grid.setColTypes("txt,txt,txt,txt");
-  Meetings.Grid.attachHeader("#text_filter,#text_filter,#select_filter,#text_filter");
+  Meetings.Grid.attachHeader("#text_filter,#text_filter,,#text_filter");
   Meetings.Grid.init();
 });
