f10fb54780ce2d9a589d134acdf0a2fe07df610c
Chris <ulrich@tcs-germany.de>
2017-08-15 12:39:41 +0200

    Fix widths

--- meeting.js
+++ meeting.js
@@ -1,9 +1,11 @@
 var Meetings;
 dhtmlxEvent(window,"load",function(){
   Meetings = newPrometList('meetings','Besprechungen');
-  Meetings.Grid.setHeader(["Nummer","Name","Status","ersteller"]);
-  Meetings.Grid.setColumnIds('ID,NAME,STATUS,CREATEDBY')
+  Meetings.Grid.setHeader(["Name","Status","ersteller"]);
+  Meetings.Grid.setColumnIds('NAME,STATUS,CREATEDBY')
   Meetings.Grid.setColTypes("txt,txt,txt,txt");
-  Meetings.Grid.attachHeader("#text_filter,#text_filter,#select_filter,#text_filter");
+  Meetings.Grid.attachHeader("#text_filter,#text_filter,#text_filter");
+  //Meetings.Grid.setColumnMinWidth('30' , 0);
+  Meetings.Grid.setInitWidths('*,70,70');
   Meetings.Grid.init();
 });
