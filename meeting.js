91e97ae4d3dc000c7f843460b93e2e2300d9637d
Christian Ulrich <info@cu-tec.de>
2017-08-18 05:24:12 +0200

    make rows readonly

--- meeting.js
+++ meeting.js
@@ -3,7 +3,7 @@ dhtmlxEvent(window,"load",function(){
   Meetings = newPrometList('meetings','Besprechungen');
   Meetings.Grid.setHeader(["Name","Status","Datum","ersteller"]);
   Meetings.Grid.setColumnIds('NAME,STATUS,DATE,CREATEDBY')
-  Meetings.Grid.setColTypes("txt,txt,txt,txt");
+  Meetings.Grid.setColTypes("ro,ro,ro,ro");
   Meetings.Grid.attachHeader("#text_filter,#text_filter,,#text_filter");
   //Meetings.Grid.setColumnMinWidth('30' , 0);
   Meetings.Grid.setInitWidths('*,70,80,70');
