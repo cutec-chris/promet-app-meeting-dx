48602a56590bc8d34d41b787e00fb6af3d9016eb
Chris <ulrich@tcs-germany.de>
2018-01-10 09:30:11 +0100

    Use Grid to show Meeting entrys

--- meeting.js
+++ meeting.js
@@ -16,6 +16,13 @@ dhtmlxEvent(window,"load",function(){
     null,       // last position
     false,      // inactive
     true);
+    aForm.MeetingGrid = aForm.Tabs.tabs("content").attachGrid();
+    aForm.MeetingGrid.setHeader(["Inhalt"]);
+    aForm.MeetingGrid.setColumnIds('desc');
+    aForm.MeetingGrid.setColTypes("ro");
+    aForm.MeetingGrid.enableMultiline(true);
+		aForm.MeetingGrid.enableAutoWidth(true);
+    aForm.MeetingGrid.init();
     aForm.Tabs.addTab(
     "users",       // id
     "Teilnehmer",    // tab text
@@ -31,15 +38,12 @@ dhtmlxEvent(window,"load",function(){
     aForm.Tabs.tabs("content").setActive();
     //aForm.Tabs.tabs("content").attachURL(GetBaseUrl()+'/meeting/by-id/'+aForm.Id+'/reports/Standart.pdf');
     aForm.OnDataUpdated = function(bForm) {
-      var aHtml = '<div id="contId" style="position: relative; width: 100%; height: 100%; overflow: auto;font-family: Roboto,Arial,Helvetica;"><ul>';
       for (var i = 0; i < bForm.Data.MEETINGENTRYS.length; i++) {
         if (bForm.Data.MEETINGENTRYS[i].Fields.link != null)
-          aHtml += '<li><b><a href=\"'+bForm.Data.MEETINGENTRYS[i].Fields.link+'\">'+bForm.Data.MEETINGENTRYS[i].Fields.desc+'</a></b></li>'
+          bForm.MeetingGrid.addRow(bForm.Data.MEETINGENTRYS[i].Fields.sql_id,['<b>'+bForm.Data.MEETINGENTRYS[i].Fields.desc.replace("\r\n","<br>")+'</b>'])
         else
-          aHtml += '<li>'+bForm.Data.MEETINGENTRYS[i].Fields.desc+'</li>';
+          bForm.MeetingGrid.addRow(bForm.Data.MEETINGENTRYS[i].Fields.sql_id,[bForm.Data.MEETINGENTRYS[i].Fields.desc.replace("\r\n","<br>")])
       }
-      aHtml += '</ul></div>';
-      aForm.Tabs.tabs("content").attachHTMLString(aHtml);
 
       for (var i = 0; i < bForm.Data.MEETINGUSERS.length; i++) {
         bForm.UsersGrid.addRow(bForm.Data.MEETINGUSERS[i].Fields.sql_id,[bForm.Data.MEETINGUSERS[i].Fields.name,bForm.Data.MEETINGUSERS[i].Fields.idcode,bForm.Data.MEETINGUSERS[i].Fields.active])
