93faed953279c8347fac08d86a66745606653cea
Chris <ulrich@tcs-germany.de>
2018-06-29 15:13:57 +0200

    Basic Form init

--- meetings.js
+++ meetings.js
@@ -1,7 +1,41 @@
-﻿rtl.module("meetings",["System","JS","Web","Classes","Avamm","webrouter","AvammForms","dhtmlx_base","SysUtils"],function () {
+﻿rtl.module("meetings",["System","JS","Web","Classes","Avamm","webrouter","AvammForms","dhtmlx_base","SysUtils","dhtmlx_grid"],function () {
   "use strict";
   var $mod = this;
   rtl.createClass($mod,"TMeetingForm",pas.AvammForms.TAvammForm,function () {
+    this.$init = function () {
+      pas.AvammForms.TAvammForm.$init.call(this);
+      this.MeetingGrid = null;
+      this.UsersGrid = null;
+    };
+    this.$final = function () {
+      this.MeetingGrid = undefined;
+      this.UsersGrid = undefined;
+      pas.AvammForms.TAvammForm.$final.call(this);
+    };
+    this.DoOnCreate = function () {
+      this.Tabs.addTab("content",rtl.getResStr(pas.meetings,"strContent"),null,0,true,false);
+      this.MeetingGrid = rtl.getObject(this.Tabs.cells("content").attachGrid(pas.JS.New([])));
+      this.MeetingGrid.setHeader("Inhalt",",",Array.of({}));
+      this.MeetingGrid.setColumnIds("desc");
+      this.MeetingGrid.setColTypes("ro");
+      this.MeetingGrid.enableMultiline(true);
+      this.MeetingGrid.enableAutoWidth(true);
+      this.MeetingGrid.enableKeyboardSupport(true);
+      this.MeetingGrid.init();
+      this.Tabs.addTab("users",rtl.getResStr(pas.meetings,"strUsers"),null,0,false,false);
+      this.UsersGrid = rtl.getObject(this.Tabs.cells("users").attachGrid(pas.JS.New([])));
+      this.UsersGrid.setHeader("Name,Code,Anwesend",",",Array.of({}));
+      this.UsersGrid.setColumnIds("name,idcode,active");
+      this.UsersGrid.setColTypes("edtxt,edtxt,edtxt");
+      this.UsersGrid.init();
+    };
+    this.DoOpen = function () {
+    };
+    this.DoLoadData = function () {
+      this.DoOnCreate();
+      this.DoOpen();
+      pas.AvammForms.TAvammForm.DoLoadData.call(this);
+    };
   });
   this.Meeting = null;
   this.ShowMeeting = function (URl, aRoute, Params) {
@@ -22,7 +56,7 @@
     };
     $mod.Meeting.Show();
   };
-  $mod.$resourcestrings = {strMeeting: {org: "Besprechungen"}};
+  $mod.$resourcestrings = {strMeeting: {org: "Besprechungen"}, strContent: {org: "Inhalt"}, strUsers: {org: "Teilnehmer"}};
   $mod.$init = function () {
     if (pas.Avamm.getRight("Meeting") > 0) pas.Avamm.RegisterSidebarRoute(rtl.getResStr(pas.meetings,"strMeeting"),"Meeting",$mod.ShowMeetingList);
     pas.webrouter.Router().RegisterRoute("\/Meeting\/by-id\/:Id\/",$mod.ShowMeeting,false);
--- meetings.lpr
+++ meetings.lpr
@@ -1,14 +1,24 @@
 library meetings;
   uses js, web, classes, Avamm, webrouter, AvammForms, dhtmlx_base,
-    dhtmlx_form,SysUtils, Types;
+    dhtmlx_form,SysUtils, Types,dhtmlx_grid;
 
 type
 
+  { TMeetingForm }
+
   TMeetingForm = class(TAvammForm)
+  private
+    MeetingGrid,UsersGrid: TDHTMLXGrid;
+  public
+    procedure DoOnCreate;
+    procedure DoOpen;
+    procedure DoLoadData; override;
   end;
 
 resourcestring
   strMeeting             = 'Besprechungen';
+  strContent             = 'Inhalt';
+  strUsers               = 'Teilnehmer';
 
 var
   Meeting : TAvammListForm = nil;
@@ -36,6 +46,38 @@ begin
         end;
     end;
   Meeting.Show;
+end;
+
+{ TMeetingForm }
+
+procedure TMeetingForm.DoOnCreate;
+begin
+  Tabs.addTab('content',strContent,null,0,true,false);
+  MeetingGrid := TDHTMLXGrid(Tabs.cells('content').attachGrid(js.new([])));
+  MeetingGrid.setHeader('Inhalt',',',TJSArray._of([]));
+  MeetingGrid.setColumnIds('desc');
+  MeetingGrid.setColTypes('ro');
+  MeetingGrid.enableMultiline(true);
+  MeetingGrid.enableAutoWidth(true);
+  MeetingGrid.enableKeyboardSupport(true);
+  MeetingGrid.init();
+  Tabs.addTab('users',strUsers,null,0,false,false);
+  UsersGrid := TDHTMLXGrid(Tabs.cells('users').attachGrid(js.new([])));
+  UsersGrid.setHeader('Name,Code,Anwesend',',',TJSArray._of([]));
+  UsersGrid.setColumnIds('name,idcode,active');
+  UsersGrid.setColTypes('edtxt,edtxt,edtxt');
+  UsersGrid.init();
+end;
+
+procedure TMeetingForm.DoOpen;
+begin
+end;
+
+procedure TMeetingForm.DoLoadData;
+begin
+  DoOnCreate;
+  DoOpen;
+  inherited DoLoadData;
 end;
 
 initialization
