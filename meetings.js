fb20e8713deb6fdad99cd749d4af1048265a17f0
Chris <ulrich@tcs-germany.de>
2018-07-10 08:04:14 +0200

    Same stand as in old vanillajs Code

--- meetings.js
+++ meetings.js
@@ -6,30 +6,63 @@
       pas.AvammForms.TAvammForm.$init.call(this);
       this.MeetingGrid = null;
       this.UsersGrid = null;
+      this.Meetingentrys = null;
+      this.Meetingusers = null;
     };
     this.$final = function () {
       this.MeetingGrid = undefined;
       this.UsersGrid = undefined;
+      this.Meetingentrys = undefined;
+      this.Meetingusers = undefined;
       pas.AvammForms.TAvammForm.$final.call(this);
     };
     this.DoOnCreate = function () {
       this.Tabs.addTab("content",rtl.getResStr(pas.meetings,"strContent"),null,0,true,false);
       this.MeetingGrid = rtl.getObject(this.Tabs.cells("content").attachGrid(pas.JS.New([])));
-      this.MeetingGrid.setHeader("Inhalt",",",Array.of({}));
-      this.MeetingGrid.setColumnIds("desc");
-      this.MeetingGrid.setColTypes("ro");
+      this.MeetingGrid.setHeader("Inhalt,Bearbeiter,Termin,Verantwortlich",",",Array.of({}));
+      this.MeetingGrid.setColumnIds("DESC,USER,DUEDATE,OWNER");
+      this.MeetingGrid.setColTypes("ro,ro,ro,ro");
+      this.MeetingGrid.setInitWidths("*,100,70,100");
       this.MeetingGrid.enableMultiline(true);
       this.MeetingGrid.enableAutoWidth(true);
       this.MeetingGrid.enableKeyboardSupport(true);
       this.MeetingGrid.init();
-      this.Tabs.addTab("users",rtl.getResStr(pas.meetings,"strUsers"),null,0,false,false);
+      this.Tabs.addTab("users",rtl.getResStr(pas.meetings,"strUsers"),null,1,false,false);
       this.UsersGrid = rtl.getObject(this.Tabs.cells("users").attachGrid(pas.JS.New([])));
       this.UsersGrid.setHeader("Name,Code,Anwesend",",",Array.of({}));
-      this.UsersGrid.setColumnIds("name,idcode,active");
+      this.UsersGrid.setColumnIds("NAME,IDCODE,ACTIVE");
       this.UsersGrid.setColTypes("edtxt,edtxt,edtxt");
       this.UsersGrid.init();
     };
     this.DoOpen = function () {
+      var i = 0;
+      var nEntry = null;
+      if (rtl.isExt(this.FData["MEETINGENTRYS"],Object,1)) {
+        this.Meetingentrys = rtl.getObject(rtl.getObject(this.FData["MEETINGENTRYS"])["Data"])}
+       else this.Meetingentrys = rtl.getObject(this.FData["MEETINGENTRYS"]);
+      if (rtl.isExt(this.FData["MEETINGUSERS"],Object,1)) {
+        this.Meetingusers = rtl.getObject(rtl.getObject(this.FData["MEETINGUSERS"])["Data"])}
+       else this.Meetingusers = rtl.getObject(this.FData["MEETINGUSERS"]);
+      this.MeetingGrid.clearAll();
+      for (var $l1 = 0, $end2 = this.Meetingentrys.length - 1; $l1 <= $end2; $l1++) {
+        i = $l1;
+        nEntry = new Array();
+        if (pas.System.Assigned(rtl.getObject(this.Meetingentrys[i])["LINK"]) && (pas.System.Copy("" + rtl.getObject(this.Meetingentrys[i])["LINK"],0,8) === "PROJECTS")) {
+          nEntry.push(("<b>" + ("" + rtl.getObject(this.Meetingentrys[i])["DESC"])) + "<\/b>")}
+         else nEntry.push("" + rtl.getObject(this.Meetingentrys[i])["DESC"]);
+        nEntry.push(rtl.getObject(this.Meetingentrys[i])["USER"]);
+        nEntry.push(rtl.getObject(this.Meetingentrys[i])["DUEDATE"]);
+        nEntry.push(rtl.getObject(this.Meetingentrys[i])["OWNER"]);
+        this.MeetingGrid.addRow(rtl.getObject(this.Meetingentrys[i])["sql_id"],nEntry);
+      };
+      for (var $l3 = 0, $end4 = this.Meetingusers.length - 1; $l3 <= $end4; $l3++) {
+        i = $l3;
+        nEntry = new Array();
+        nEntry.push("" + rtl.getObject(this.Meetingusers[i])["NAME"]);
+        nEntry.push(rtl.getObject(this.Meetingusers[i])["IDCODE"]);
+        nEntry.push(rtl.getObject(this.Meetingusers[i])["ACTIVE"]);
+        this.UsersGrid.addRow(rtl.getObject(this.Meetingusers[i])["sql_id"],nEntry);
+      };
     };
     this.DoLoadData = function () {
       this.DoOnCreate();
@@ -40,13 +73,13 @@
   this.Meeting = null;
   this.ShowMeeting = function (URl, aRoute, Params) {
     var aForm = null;
-    aForm = $mod.TMeetingForm.$create("Create$1",[pas.AvammForms.TAvammFormMode.fmInlineWindow,"Meeting",Params.GetValue("Id"),""]);
+    aForm = $mod.TMeetingForm.$create("Create$1",[pas.AvammForms.TAvammFormMode.fmInlineWindow,"meetings",Params.GetValue("Id"),""]);
   };
   this.ShowMeetingList = function (URl, aRoute, Params) {
     var aParent = null;
     if ($mod.Meeting === null) {
       aParent = rtl.getObject(pas.Avamm.GetAvammContainer());
-      $mod.Meeting = pas.AvammForms.TAvammListForm.$create("Create$1",[aParent,"Meeting"]);
+      $mod.Meeting = pas.AvammForms.TAvammListForm.$create("Create$1",[aParent,"meetings","1C"]);
       var $with1 = $mod.Meeting;
       $with1.Grid.setHeader("Name,Status,Datum,ersteller",",",Array.of({}));
       $with1.Grid.setColumnIds("NAME,STATUS,DATE,CREATEDBY");
@@ -58,8 +91,8 @@
   };
   $mod.$resourcestrings = {strMeeting: {org: "Besprechungen"}, strContent: {org: "Inhalt"}, strUsers: {org: "Teilnehmer"}};
   $mod.$init = function () {
-    if (pas.Avamm.getRight("Meeting") > 0) pas.Avamm.RegisterSidebarRoute(rtl.getResStr(pas.meetings,"strMeeting"),"Meeting",$mod.ShowMeetingList);
-    pas.webrouter.Router().RegisterRoute("\/Meeting\/by-id\/:Id\/",$mod.ShowMeeting,false);
+    if (pas.Avamm.getRight("meetings") > 0) pas.Avamm.RegisterSidebarRoute(rtl.getResStr(pas.meetings,"strMeeting"),"Meeting",$mod.ShowMeetingList);
+    pas.webrouter.Router().RegisterRoute("\/meetings\/by-id\/:Id\/",$mod.ShowMeeting,false);
   };
 });
 //# sourceMappingURL=meetings.js.map
--- meetings.lpr
+++ meetings.lpr
@@ -9,6 +9,8 @@ type
   TMeetingForm = class(TAvammForm)
   private
     MeetingGrid,UsersGrid: TDHTMLXGrid;
+    Meetingentrys : TJSArray;
+    Meetingusers : TJSArray;
   public
     procedure DoOnCreate;
     procedure DoOpen;
@@ -26,7 +28,7 @@ Procedure ShowMeeting(URl : String; aRoute : TRoute; Params: TStrings);
 var
   aForm: TAvammForm;
 begin
-  aForm := TMeetingForm.Create(fmInlineWindow,'Meeting',Params.Values['Id']);
+  aForm := TMeetingForm.Create(fmInlineWindow,'meetings',Params.Values['Id']);
 end;
 Procedure ShowMeetingList(URl : String; aRoute : TRoute; Params: TStrings);
 var
@@ -35,7 +37,7 @@ begin
   if Meeting = nil then
     begin
       aParent := TJSHTMLElement(GetAvammContainer());
-      Meeting := TAvammListForm.Create(aParent,'Meeting');
+      Meeting := TAvammListForm.Create(aParent,'meetings');
       with Meeting do
         begin
           Grid.setHeader('Name,Status,Datum,ersteller',',',TJSArray._of([]));
@@ -54,23 +56,56 @@ procedure TMeetingForm.DoOnCreate;
 begin
   Tabs.addTab('content',strContent,null,0,true,false);
   MeetingGrid := TDHTMLXGrid(Tabs.cells('content').attachGrid(js.new([])));
-  MeetingGrid.setHeader('Inhalt',',',TJSArray._of([]));
-  MeetingGrid.setColumnIds('desc');
-  MeetingGrid.setColTypes('ro');
+  MeetingGrid.setHeader('Inhalt,Bearbeiter,Termin,Verantwortlich',',',TJSArray._of([]));
+  MeetingGrid.setColumnIds('DESC,USER,DUEDATE,OWNER');
+  MeetingGrid.setColTypes('ro,ro,ro,ro');
+  MeetingGrid.setInitWidths('*,100,70,100');
   MeetingGrid.enableMultiline(true);
   MeetingGrid.enableAutoWidth(true);
   MeetingGrid.enableKeyboardSupport(true);
   MeetingGrid.init();
-  Tabs.addTab('users',strUsers,null,0,false,false);
+  Tabs.addTab('users',strUsers,null,1,false,false);
   UsersGrid := TDHTMLXGrid(Tabs.cells('users').attachGrid(js.new([])));
   UsersGrid.setHeader('Name,Code,Anwesend',',',TJSArray._of([]));
-  UsersGrid.setColumnIds('name,idcode,active');
+  UsersGrid.setColumnIds('NAME,IDCODE,ACTIVE');
   UsersGrid.setColTypes('edtxt,edtxt,edtxt');
   UsersGrid.init();
 end;
 
 procedure TMeetingForm.DoOpen;
+var
+  i: Integer;
+  nEntry: TJSArray;
 begin
+  if Data.Properties['MEETINGENTRYS'] is TJSObject then
+    Meetingentrys := TJSArray(TJSObject(Data.Properties['MEETINGENTRYS']).Properties['Data'])
+  else
+    Meetingentrys := TJSArray(Data.Properties['MEETINGENTRYS']);
+  if Data.Properties['MEETINGUSERS'] is TJSObject then
+    Meetingusers := TJSArray(TJSObject(Data.Properties['MEETINGUSERS']).Properties['Data'])
+  else
+    Meetingusers := TJSArray(Data.Properties['MEETINGUSERS']);
+  MeetingGrid.clearAll;
+  for i := 0 to Meetingentrys.Length-1 do
+    begin
+      nEntry := TJSArray.new;
+      if Assigned(TJSObject(Meetingentrys[i]).Properties['LINK']) and (copy(string(TJSObject(Meetingentrys[i]).Properties['LINK']),0,8)='PROJECTS') then
+        nEntry.push('<b>'+string(TJSObject(Meetingentrys[i]).Properties['DESC'])+'</b>')
+      else
+        nEntry.push(string(TJSObject(Meetingentrys[i]).Properties['DESC']));
+      nEntry.push(TJSObject(Meetingentrys[i]).Properties['USER']);
+      nEntry.push(TJSObject(Meetingentrys[i]).Properties['DUEDATE']);
+      nEntry.push(TJSObject(Meetingentrys[i]).Properties['OWNER']);
+      MeetingGrid.addRow(TJSObject(Meetingentrys[i]).Properties['sql_id'],nEntry);
+    end;
+  for i := 0 to Meetingusers.Length-1 do
+    begin
+      nEntry := TJSArray.new;
+      nEntry.push(string(TJSObject(Meetingusers[i]).Properties['NAME']));
+      nEntry.push(TJSObject(Meetingusers[i]).Properties['IDCODE']);
+      nEntry.push(TJSObject(Meetingusers[i]).Properties['ACTIVE']);
+      UsersGrid.addRow(TJSObject(Meetingusers[i]).Properties['sql_id'],nEntry);
+    end;
 end;
 
 procedure TMeetingForm.DoLoadData;
@@ -81,8 +116,8 @@ begin
 end;
 
 initialization
-  if getRight('Meeting')>0 then
+  if getRight('meetings')>0 then
     RegisterSidebarRoute(strMeeting,'Meeting',@ShowMeetingList);
-  Router.RegisterRoute('/Meeting/by-id/:Id/',@ShowMeeting);
+  Router.RegisterRoute('/meetings/by-id/:Id/',@ShowMeeting);
 end.
 
